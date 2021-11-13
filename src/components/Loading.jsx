0b50c59485793f4d54df3a4926c5e9cafc9a2f03
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-11-13 11:09:59 -0300

    Requisito 2 - page login

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <div className="loading">
+        <h2>Carregando...</h2>
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,95 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
+// import Button from '../components/Button';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: false,
+      ok: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // this.validateButton = this.validateButton.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+  }
+
+  // componentDidMount() {
+  //   console.log(createUser(this.state));
+  // }
+
+  handleChange({ target }) {
+    this.setState({
+      name: target.value,
+    });
+  }
+
+  handleClick() {
+    const { name } = this.state;
+    this.setState({ loading: true }, () => {
+      createUser({ name })
+        .then(() => this.setState({ ok: true }));
+    });
+  }
+
+  validateInput() {
+    const { loading } = this.state;
+    const inputName = (
+      <input
+        data-testid="login-name-input"
+        type="text"
+        placeholder="Nome de Usuario"
+        onChange={ this.handleChange }
+      />
+    );
+
+    if (loading) {
+      return <Loading />;
+    }
+    return inputName;
+  }
+
+  // validateButton() {
+  //   if (name.length < minLenght) return disabled;
+  //   if (name.length >= minLenght) return enabled;
+  // }
+
   render() {
+    const minLenght = 3;
+    const disabled = (
+      <button
+        type="button"
+        data-testid="login-submit-button"
+        click={ this.handleClick }
+        name="Entrar"
+        disabled="true"
+      >
+        Entrar
+      </button>);
+    const enabled = (
+      <button
+        type="button"
+        data-testid="login-submit-button"
+        onClick={ this.handleClick }
+        name="Entrar"
+      >
+        Entrar
+      </button>);
+    const { name, ok } = this.state;
     return (
       <div data-testid="page-login">
-        <p>Tela de Login</p>
+        <form>
+          <fieldset>
+            {this.validateInput()}
+            {name.length >= minLenght ? enabled : disabled}
+            {ok === true && <Redirect to="/search" />}
+          </fieldset>
+        </form>
       </div>
     );
   }
