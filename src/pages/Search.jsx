6bc28225f3a4c6b93fe9a1e602c5752029bd7de9
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-11-13 14:20:55 -0300

    Requisito 5 - Form Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,57 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artist: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      artist: target.value,
+    });
+  }
+
   render() {
+    const { artist } = this.state;
+    const minLength = 2;
+
+    const enabled = (
+      <button
+        data-testid="search-artist-button"
+        type="button"
+      >
+        Pesquisar
+      </button>);
+
+    const disabled = (
+      <button
+        data-testid="search-artist-button"
+        type="button"
+        disabled="true"
+      >
+        Pesquisar
+      </button>);
+
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Tela de Search</p>
+        <div className="form-search">
+          <form>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              placeholder="Nome do Artista"
+              onChange={ this.handleChange }
+            />
+            { artist.length >= minLength ? enabled : disabled}
+          </form>
+        </div>
       </div>
     );
   }
